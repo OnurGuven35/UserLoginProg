eddfec1e2485b79ef201ec2f38316995357be455
OnurGuven35 <gvnn.onrr@gmail.com>
2022-06-21 15:11:17 +0300

    .

--- src/User.java
+++ src/User.java
@@ -4,37 +4,43 @@ public class User {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String userName, password, secim;
+        String userName, password, select;
 
+        // İlk olarak kullanıcıdan girişleri alıyoruz. Eğer doğru ise program bitiyor. Yanlış ise şifre yenilemek isteyip istemediğini soruyoruz.
+
+        System.out.println();
         System.out.print("Lutfen Kullanici Adinizi Giriniz : ");
         userName = input.nextLine();
         System.out.print("Lutfen Sifrenizi Giriniz : ");
         password = input.nextLine();
 
         if (userName.equals("patika") && password.equals("java123")) {
-            System.out.print("Girisiniz Yapilmistir ! ");
+            System.out.print("\nGirisiniz Yapilmistir ! ");
         } else {
-            System.out.println("Kullanici Adiniz veya Sifreniz Yanlistir ! ");
+            System.out.println("\nKullanici Adiniz veya Sifreniz Yanlistir ! ");
             System.out.println("Sifrenizi Sifirlamak Istermisiniz ?");
             System.out.print("Evet veya Hayiri Tuslayiniz : ");
         }
 
-        secim = input.nextLine();
+        select = input.nextLine();
 
-        switch (secim) {
-            case "Evet" -> {
-                System.out.print("Lutfen Yeni Bir Sifre Belirleyiniz  : ");
+        // Eğer cevap evetse yeni şifreyi oluşturuyoruz. Hayırsa program bitiyor.
+
+        switch (select) {
+            case "Evet":
+                System.out.print("\nLutfen Yeni Bir Sifre Belirleyiniz  : ");
                 String newPass;
                 newPass = input.nextLine();
 
                 if (newPass.equals("java123")) {
-                    System.out.print("Yeni Sifre Bir Oncekiyle Ayni Olamaz ! ");
+                    System.out.println("\nYeni Sifre Bir Oncekiyle Ayni Olamaz ! ");
                 } else {
-                    System.out.print("Yeni Sifreniz Basariyla Olusturulmustur ! ");
+                    System.out.println("\nYeni Sifreniz Basariyla Olusturulmustur ! ");
                 }
-            }
-            case "Hayir" -> System.out.print("Giris Sayfasina Yonlendiriliyorsunuz !");
+                break;
+            case "Hayir":
+                System.out.println("\nGiris Sayfasina Yonlendiriliyorsunuz !");
+                break;
         }
     }
 }
-
